a62ec689ed933869f02e0dc0f944b69e587a1e68
Xavier Vieujean <xavier.vieujean@hotmail.ch>
2021-01-24 13:46:36 +0100

    add services & contact static home pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,14 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :services, :contact ]
 
   def home
   end
+
+  def services
+  end
+
+  def contact
+  end
+
+
 end
